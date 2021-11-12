b5963db51facd219bc6d6541f6e5c65318d86113
Bryan <bryanvargasnegrete@gmail.com>
2021-11-12 19:41:47 +0100

    Proyecto android añadido

---
+++ AplicacionTFG/app/src/main/java/com/example/heartapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.heartapp;
+
+import android.app.Activity;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
